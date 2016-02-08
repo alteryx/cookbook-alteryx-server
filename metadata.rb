f75c34db8bb2c05cd5694f4df8713450d9ac458b
Jake Migler <jmigler@alteryx.com>
2016-02-08 15:35:55 -0700

    Add Windows dependency

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,5 @@ license          'All rights reserved'
 description      'Installs/Configures Alteryx server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.0.0'
+supports         'windows'
+depends          'windows', '~> 1.39.0'
