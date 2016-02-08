a237b65306ac1fe4886e0e967255f44678ab1a40
Jake Migler <jmigler@alteryx.com>
2016-02-08 15:44:04 -0700

    Relax Windows dependency

--- metadata.rb
+++ metadata.rb
@@ -6,4 +6,4 @@ description      'Installs/Configures Alteryx server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.0.0'
 supports         'windows'
-depends          'windows', '~> 1.39.0'
+depends          'windows'
