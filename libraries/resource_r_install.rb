efc2ca13ec5c08cbbafe94fcd7d3138a27fc4f29
Jake Migler <jmigler@alteryx.com>
2016-03-08 15:58:09 -0700

    Add r_install LWRP

---
+++ libraries/resource_r_install.rb
@@ -0,0 +1,18 @@
+module AlteryxServer
+  # Chef resource for installing R Predictive tools
+  class RInstallResource < Chef::Resource::LWRPBase
+    resource_name :r_install
+    provides :r_install
+
+    actions(:install)
+    default_action :install
+
+    attribute(
+      :source,
+      kind_of: String,
+      default: lazy do
+        AlteryxServer::Helpers.exe_glob('C:/Program Files/Alteryx/RInstaller/')
+      end
+    )
+  end
+end
