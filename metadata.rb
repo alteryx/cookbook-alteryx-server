203a831d701c1586835b4b9436792d0ee2fbda23
Jake Migler <jmigler@alteryx.com>
2016-05-20 10:22:25 -0600

    Add license

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'alteryx-server'
 maintainer       'Alteryx, Inc.'
 maintainer_email ''
-license          'All rights reserved'
+license          'Apache 2.0'
 description      'Installs/Configures Alteryx server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.11.1'
