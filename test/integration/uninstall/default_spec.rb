a27ebf597031c47cd7c65163e5e4034f19872fcc
Nathan Warner <nwarner@alteryx.com>
2017-03-16 10:56:20 -0600

    fixing unit tests

--- test/integration/uninstall/default_spec.rb
+++ test/integration/uninstall/default_spec.rb
@@ -1,7 +1,7 @@
 describe package('Alteryx 11.0 x64') do
-  it { should not be_installed }
+  it { should_not be_installed }
 end
 
 describe package('Alteryx Predictive Tools with R 3.3.2') do
-  it { should not be_installed }
+  it { should_not be_installed }
 end
